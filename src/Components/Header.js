e51305abd1f62ad59319df972d135d285d71fdc2
Imran Syed <imran@Imrans-MacBook-Air.local>
2022-05-19 17:36:16 -0500

    functional navbar for mobile

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,36 +1,40 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { FaBars } from "react-icons/fa";
 
 const Header = () => {
 
+  const [showMenu, setShowMenu] = useState(true)
+
+  let menu = ''
+
+  if(showMenu){
+    menu =  
+        <ul>
+            <Link to="/" className="link-header">
+              <li>Home</li>
+            </Link>
+
+            <Link to="/readinglist" className="link-header">
+              <li>Reading List</li>
+            </Link>
+
+            <Link to="/about" className="link-header">
+              <li>About</li>
+            </Link>
+        </ul>
+    }
 
 
- const handleClickNav = () => {
-   console.log('hello');
- }
  
   return (
     <nav className="header-nav">
 
-      <a onClick={handleClickNav} id="toggle-button" href="#">
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
+      <a onClick={()=>setShowMenu(!showMenu)} className="toggle-button" href="#"> 
+          <FaBars className="bars"/>
       </a>
 
-      <ul>
-        <Link to="/" className="link-header">
-          <li>Home</li>
-        </Link>
-
-        <Link to="/readinglist" className="link-header">
-          <li>Reading List</li>
-        </Link>
-
-        <Link to="/about" className="link-header">
-          <li>About</li>
-        </Link>
-      </ul>
+      {menu}
 
     </nav>
   );
