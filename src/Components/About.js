e3ac514b88eacf21177f5bc03ca246dd23e16ccf
Imran Syed <imran@Imrans-MacBook-Air.local>
2022-05-19 15:40:40 -0500

    working on burger menu

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,7 @@ const About  = () => {
         <nav className='about-nav'>
             <ul>
                 <li>This website allows the user to find current news articles from over 80,000 news sources </li>
-                <li>Choose a topic you are interested in and enter the date to find the most current news articles from the most popular sources</li>
+                <li>Choose a topic of interest and enter the date to find the most current news articles from the most popular sources</li>
                 <li>If you are interested in an article, go ahead and save the article into your personal reading list!</li>
             </ul>
         </nav>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,27 +1,39 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
-    return(
-        <nav className='header-nav'>
-          <ul>
 
-          <Link to='/' className='link-header'>
+
+
+ const handleClickNav = () => {
+   console.log('hello');
+ }
+ 
+  return (
+    <nav className="header-nav">
+
+      <a onClick={handleClickNav} id="toggle-button" href="#">
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </a>
+
+      <ul>
+        <Link to="/" className="link-header">
           <li>Home</li>
-          </Link>
+        </Link>
 
-          <Link to='/readinglist'className="link-header" >
+        <Link to="/readinglist" className="link-header">
           <li>Reading List</li>
-          </Link>
+        </Link>
 
-          <Link to='/about' className="link-header">
+        <Link to="/about" className="link-header">
           <li>About</li>
-          </Link>
+        </Link>
+      </ul>
 
-          </ul>
-      </nav>
-
-    )
-}
-
+    </nav>
+  );
+};
 
 export default Header;
